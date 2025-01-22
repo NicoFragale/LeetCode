366a50a278ad6ce55ebf1f8466c0794e4f321c35
NicoFragale <nico.fragale2002@gmail.com>
2025-01-22 12:02:19 +0100

    Aggiornamenti

---
+++ MySQL/176.sql
@@ -0,0 +1,20 @@
+'''
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+'''
+
+select (select distinct salary
+from Employee 
+order by salary DESC
+limit 1 offset 1 ) as SecondHighestSalary
